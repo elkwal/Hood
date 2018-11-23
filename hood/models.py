8e662ae9711722112c727c248d14b6fdb5ef0164
Gift-Lumumba <giftlumumba2@gmail.com>
2018-11-23 10:02:13 +0300

    added registration form

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    neighbourhood_name = models.CharField(max_length= 50)
+    neighbourhood_location = models.CharField(max_length=50)
+    occupant_count=models.IntegerField()
+    admin = models.ForeignKey(User,null=True,on_delete=models.CASCADE)
+
