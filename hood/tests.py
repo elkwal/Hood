4f90c8490f0fa05300a9173eef3891270cabb4a7
Ekiru Timothy <ekiru@outlook.com>
2018-11-26 00:39:20 +0300

    made changes

--- hood/tests.py
+++ hood/tests.py
@@ -20,10 +20,10 @@ class BusinessTestClass(TestCase):
 
     # Testing  instance
     def test_instance(self):
-      
         self.assertTrue(isinstance(self.new_business,Business))
 
     #Testing Save Method
+    
     def test_save_method(self):
     
         business = Business.objects.all()
