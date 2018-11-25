8e701002519eb62e0519c224942583bcf8dbe8eb
Ekiru Timothy <ekiru@outlook.com>
2018-11-25 23:32:18 +0300

    debugged my code

--- project/urls.py
+++ project/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+
 from django.conf.urls import url,include
 from django.contrib import admin
 from django.contrib.auth import views 
@@ -22,5 +23,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('hood.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
-
+  
 ]
