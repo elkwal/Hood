0c3ad9d022591df7b2eb90361e4089aa27427e6c
Ekiru Timothy <ekiru@outlook.com>
2018-11-25 22:35:45 +0300

    commit changes made

--- hood/admin.py
+++ hood/admin.py
@@ -1,5 +1,10 @@
+
 from django.contrib import admin
-from .models import Neighbourhood
+from .models import Business,User_profile,Neigbourhood,Post
 
 # Register your models here.
-admin.site.register(Neighbourhood)
+
+admin.site.register(Business)
+admin.site.register(User_profile)
+admin.site.register(Neigbourhood)
+admin.site.register(Post)
